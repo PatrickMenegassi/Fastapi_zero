d40a32e7e5a138164c3709376ff7f002262bf637
Patrick <pmwalter@inf.ufpel.edu.br>
2026-02-10 16:36:01 -0300

    Atualizando endpoints para usar o banco dados

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,17 +7,18 @@ from fastapi_zero.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(username='test', password='secret', email='teste@test')
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
         session.add(new_user)
         session.commit()
 
-        user = session.scalar(select(User).where(User.username == 'test'))
-        session.add(user)
+    user = session.scalar(select(User).where(User.username == 'alice'))
 
     assert asdict(user) == {
         'id': 1,
-        'username': 'test',
-        'email': 'test@test',
+        'username': 'alice',
         'password': 'secret',
+        'email': 'teste@test',
         'created_at': time,
     }
